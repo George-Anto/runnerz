2aca2af56e2f4f6cddfa25b9c68f851f24aad401
George-Anto <george3osfp@gmail.com>
2024-10-15 00:08:02 +0300

    Corrections

--- runnerz-app/src/assets/env.js
+++ runnerz-app/src/assets/env.js
@@ -1,6 +1,8 @@
 (function (window) {
-  window["env"] = window["env"] || {};
+  // window["env"] = window["env"] || {};
+  window.env = window.env || {};
 
   //Environment variables
-  window["env"]["NG_APP_SERVER_URL"] = "${NG_APP_SERVER_URL}";
+  // window["env"]["NG_APP_SERVER_URL"] = "${NG_APP_SERVER_URL}";
+  window.env.NG_APP_SERVER_URL = "${NG_APP_SERVER_URL}";
 })(this);
